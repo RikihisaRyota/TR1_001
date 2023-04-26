9e78b20017d51ecabb9268ee73a7716d84aa7d69
RikihisaRyota <115054910+RikihisaRyota@users.noreply.github.com>
2023-04-26 10:50:37 +0900

    座標変換なし

--- DrawWall.cpp
+++ DrawWall.cpp
@@ -129,7 +129,7 @@ void DrawWall::Update(const Player& player, const Wall& wall)
 	//左真ん中壁
 	SetMatrix(middleRightWallWorld_, middleRightWallScale_, middleRightWallRotate_, middleRightWallTransform_, scale_, rotateRight_, { trapezoidCenter_.x+336.0f, trapezoidCenter_.y+218.0f });
 
-	/*for (size_t i = 0; i < Wall::kMax; i++) {
+	for (size_t i = 0; i < Wall::kMax; i++) {
 		switch (i)
 		{
 		case Wall::backLeft:
@@ -205,7 +205,7 @@ void DrawWall::Update(const Player& player, const Wall& wall)
 			}
 			break;
 		}
-	}*/
+	}
 
 	//デバック
 	memcpy(m_PreKeys, m_Keys, 256);
