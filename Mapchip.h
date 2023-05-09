441affa649657bc4e50ee4e323806e54c92e8c57
RikihisaRyota <115054910+RikihisaRyota@users.noreply.github.com>
2023-05-09 21:46:08 +0900

    サイズ変更

--- Mapchip.h
+++ Mapchip.h
@@ -5,7 +5,7 @@ class Mapchip
 public:
 	static const int kMapChipWidth = 12;
 	static const int kMapChipHeight = 12;
-	static const int kChipSize = 20;
+	static const int kChipSize = 40;
 
 	int GetMapchip(Vec2 pos) { return m_Mapchip[static_cast<int>(pos.y)][static_cast<int>(pos.x)]; }
 	Mapchip();
